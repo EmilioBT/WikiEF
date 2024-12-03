bdd0db5e9dcd2aa2b04efe6b6a49108c0a9ff74c
EmilioBT <emilio7blanco@gmail.com>
2024-12-03 12:18:16 +0100

    Error en la lectura en la web de las descripciones de tanto los hábitos como las herramientas debido a que no leía correctamente el html, esto ocurre porque  no se está renderizando el contenido correctamente en las plantillas de django. Hay que aplicar  |safe

--- Web_Proyecto/herramientas/tests.py
+++ Web_Proyecto/herramientas/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Herramienta
+from django.utils.text import slugify
 
-# Create your tests here.
+
+class HerramientaListViewTest(TestCase):
+    def setUp(self):
+        self.herramienta1 = Herramienta.objects.create(
+            title="Herramienta 1", 
+            description="Descripción de prueba 1"
+        )
+        self.herramienta2 = Herramienta.objects.create(
+            title="Herramienta 2", 
+            description="Descripción de prueba 2"
+        )
+    
+    def test_herramientas_list_view(self):
+        response = self.client.get(reverse('herramientas:herramientas'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Herramienta 1")
+        self.assertContains(response, "Herramienta 2")
+
+class HerramientaDetailViewTest(TestCase):
+    def setUp(self):
+        self.herramienta = Herramienta.objects.create(
+            title="Herramienta 1", 
+            description="Descripción de prueba"
+        )
+        self.page_slug = slugify(self.herramienta.title)
+    
+    def test_herramienta_detail_view(self):
+        response = self.client.get(reverse('herramientas:herramientas', args=[self.herramienta.id, self.page_slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Herramienta 1")
+        self.assertContains(response, "Descripción de prueba")
